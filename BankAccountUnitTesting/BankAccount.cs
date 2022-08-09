442a8f46f617651d722c6251df3224484a595357
Tanish Brichpuria <109622014+tanish8712@users.noreply.github.com>
2022-08-09 10:55:17 +0530

    Add files via upload

---
+++ BankAccountUnitTesting/BankAccount.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountUnitTesting
+{
+   public class BankAccount
+    {
+        private double balance;
+        private string name;
+        public BankAccount()
+        {
+        }
+
+        public BankAccount(string name, double balance)
+        {
+            this.balance = balance;
+            this.name = name;
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+        }
+
+        public void Add(string name, double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            }
+
+            balance += amount;
+        }
+
+        public void Withdraw(string name, double amount)
+        {
+            if (amount > balance)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            }
+
+            balance -= amount;
+        }
+
+        public void TransferBalance(string name, BankAccount otherAccount, double amount)
+        {
+            if (otherAccount is null)
+            {
+                throw new ArgumentNullException(nameof(otherAccount));
+            }
+
+            Withdraw(name, amount);
+            otherAccount.Add(name, amount);
+        }
+    }
+}
---
+++ BankAccountUnitTesting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountUnitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\tBankAccountUnitTesting");
+            var acc = new BankAccount("Rajveer", 5000);
+            Console.WriteLine("Account Holder Name : {0}", acc.Name);
+            Console.Write("Amount: ");
+            Console.WriteLine(acc.Balance);
+            acc.Add("Rajveer", 500);
+            Console.Write("\nUpdated Amount: ");
+            Console.WriteLine(acc.Balance);
+            Console.ReadLine();
+        }
+    }
+}
